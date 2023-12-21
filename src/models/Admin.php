1f1b4ddb46efc8c4744fd128b4929551da4674bb
Tu Thi Hue <21522109@gm.uit.edu.vn>
2023-12-22 01:36:23 +0700

    CRUD customer, room, booking management

---
+++ src/models/Admin.php
@@ -0,0 +1,371 @@
+<?php
+include "../controllers/inc/db_config.php";
+
+$action = isset($_GET['action']) ? $_GET['action'] : '';
+
+switch ($action) {
+    case 'listcustomer':
+        $sql = "SELECT * FROM customers";
+        $result = mysqli_query($con, $sql);
+
+        if ($result) {
+            $html = ''; // Variable to store the generated HTML
+
+            while ($row = mysqli_fetch_assoc($result)) {
+                $html .= '<tr>';
+                $html .= '<td>' . $row['customerID'] . '</td>';
+                $html .= '<td>' . $row['customerFirstName'] . '</td>';
+                $html .= '<td>' . $row['customerLastName'] . '</td>';
+                $html .= '<td>' . $row['customerDob'] . '</td>';
+                $html .= '<td>' . $row['customerEmail'] . '</td>';
+                $html .= '<td>' . $row['customerPhoneNumber'] . '</td>';
+                $html .= '<td>' . $row['customerAddress'] . '</td>';
+                $html .= '<td><div class="d-flex">';
+                $html .= '<a href="#viewCustomerModal" class="m-1 view" data-toggle="modal" onclick="viewCustomer(' . $row['customerID'] . ')"><i class="fa" data-toggle="tooltip" title="view">&#xf06e;</i></a>';
+                $html .= '<a href="#editCustomerModal" class="m-1 edit" data-toggle="modal" onclick=viewCustomer("' . $row['customerID'] . '")><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>';
+                $html .= '<a href="#deleteCustomerModal" class="m-1 delete" data-toggle="modal" onclick="prepareAction(' . $row['customerID'] . ')"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>';
+                $html .= '</div></td>';
+                $html .= '</tr>';
+            }
+
+            // Echo the generated HTML
+            echo $html;
+        } else {
+            echo 'Error executing SQL query: ' . mysqli_error($con);
+        }
+    break;
+
+    case 'listbooking':
+        $sql = "SELECT bookings.bookingID, customers.customerFirstName, customers.customerLastName, rooms.roomID, rooms.roomName, bookings.checkinDate, bookings.checkOutDate, bookings.paymentStatus
+        FROM bookings
+        JOIN rooms ON bookings.roomID = rooms.roomID
+        JOIN customers ON bookings.customerID = customers.customerID";
+
+        $result = mysqli_query($con, $sql);
+        if ($result) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo '<tr>';
+                echo '<td>' . $row['bookingID'] . '</td>';
+                echo '<td>' . $row['customerFirstName'] . '</td>';
+                echo '<td>' . $row['customerLastName'] . '</td>';
+                echo '<td>' . $row['roomID'] . '</td>';
+                echo '<td>' . $row['roomName'] . '</td>';
+                echo '<td>' . $row['checkinDate'] . '</td>';
+                echo '<td>' . $row['checkOutDate'] . '</td>';
+                echo '<td>' . $row['paymentStatus'] . '</td>';
+                echo '<td>';
+                echo '<div class="d-flex">';
+                echo '<a href="#viewBookingModal" class="m-1 view" data-toggle="modal" onclick="viewBooking(' . $row['bookingID'] . ')">
+                        <i class="fa" data-toggle="tooltip" title="view">&#xf06e;</i>
+                    </a>';            
+                echo '<a href="#editBookingModal" class="m-1 edit" data-toggle="modal" onclick="viewBooking(' . $row['bookingID'] . ')">
+                        <i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i>
+                    </a>';
+                echo '<a href="#deleteBookingModal" class="m-1 delete" data-toggle="modal" onclick="prepareAction(' . $row['bookingID'] . ')">
+                        <i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i>
+                    </a>';
+                echo '</div>';
+                echo '</td>';
+                echo '</tr>';
+            }
+            
+        } else {
+            echo 'Error executing SQL query: ' . mysqli_error($con);
+        }
+    break;
+    case 'listroom':
+        $sql = "SELECT * FROM rooms";
+        $result = mysqli_query($con, $sql);
+        if ($result) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo '<tr>';
+                echo '<td>' . $row['roomID'] . '</td>';
+                echo '<td>' . $row['roomName'] . '</td>';
+                echo '<td>' . $row['roomType'] . '</td>';
+                echo '<td>' . $row['roomRate'] . '</td>';
+                echo '<td>' . $row['roomStatus'] . '</td>';
+                echo '<td>';
+                echo '<div class="d-flex">';
+                echo '<a href="#viewRoomModal" class="m-1 view" data-toggle="modal" onclick="viewRoom(' . $row['roomID'] . ')">
+                        <i class="fa" data-toggle="tooltip" title="view">&#xf06e;</i>
+                    </a>';            
+                echo '<a href="#editRoomModal" class="m-1 edit" data-toggle="modal" onclick="viewRoom(' . $row['roomID'] . ')">
+                        <i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i>
+                    </a>';
+                echo '<a href="#deleteRoomModal" class="m-1 delete" data-toggle="modal" onclick="prepareAction(' . $row['roomID'] . ')">
+                        <i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i>
+                    </a>';
+                echo '</div>';
+                echo '</td>';
+                echo '</tr>';
+            }
+            
+        } else {
+            echo 'Error executing SQL query: ' . mysqli_error($con);
+        }
+    break;
+    case 'deleteCustomer':
+        $id =$_GET['idcustomer'];  
+        $sql= "DELETE FROM  customers WHERE customerID  = '$id' " ; 
+
+        if (mysqli_query($con, $sql)) {
+            echo "Record deleted successfully";
+        } else {
+            echo "Error deleting record: " . mysqli_error($con);
+        }
+    break;
+    case 'deleteBooking':
+        $id =$_GET['idbooking'];  
+        $sql= "DELETE FROM  bookings WHERE bookingID  = '$id' " ; 
+
+        if (mysqli_query($con, $sql)) {
+            echo "Record deleted successfully";
+        } else {
+            echo "Error deleting record: " . mysqli_error($con);
+        }
+    break;
+    case 'deleteRoom':
+        $id =$_GET['idroom'];  
+        $sql= "DELETE FROM  rooms WHERE roomID  = '$id' " ; 
+
+        if (mysqli_query($con, $sql)) {
+            echo "Record deleted successfully";
+        } else {
+            echo "Error deleting record: " . mysqli_error($con);
+        }
+    break;
+    case 'viewCustomer':
+        $id =$_GET['idcustomer'];  
+        $sql = "SELECT * FROM customers WHERE customerID = '$id'";
+        $result = mysqli_query($con, $sql);
+    
+        if ($result) {
+            $customerData = mysqli_fetch_assoc($result);
+    
+            // Return customer data as JSON
+            echo json_encode($customerData);
+        } else {
+            // Handle the error if needed
+            echo 'Error executing SQL query: ' . mysqli_error($con);
+        }
+        
+    break;
+    case 'viewRoom':
+        $id =$_GET['idroom'];  
+        $sql = "SELECT * FROM rooms WHERE roomID = '$id'";
+        $result = mysqli_query($con, $sql);
+    
+        if ($result) {
+            $roomData = mysqli_fetch_assoc($result);
+    
+            // Return customer data as JSON
+            echo json_encode($roomData);
+        } else {
+            // Handle the error if needed
+            echo 'Error executing SQL query: ' . mysqli_error($con);
+        }
+        
+    break;
+    case 'viewBooking':
+        $id =$_GET['idbooking'];  
+        $sql = "SELECT bookings.bookingID, customers.customerFirstName, customers.customerLastName, rooms.roomID, rooms.roomName, bookings.checkinDate, bookings.checkOutDate, bookings.paymentStatus,  bookings.totalAmount, bookings.numberOfCustomer, bookings.message
+        FROM bookings
+        JOIN rooms ON bookings.roomID = rooms.roomID
+        JOIN customers ON bookings.customerID = customers.customerID
+        WHERE bookingID = '$id'";
+        $result = mysqli_query($con, $sql);
+    
+        if ($result) {
+            $BookingData = mysqli_fetch_assoc($result);
+    
+            // Return customer data as JSON
+            echo json_encode($BookingData);
+        } else {
+            // Handle the error if needed
+            echo 'Error executing SQL query: ' . mysqli_error($con);
+        }
+        
+    break;
+    case 'addCustomer':
+        $firstName = $_GET['firstName'];
+        $lastName = $_GET['lastName'];
+        $dob = $_GET['dob'];
+        $email = $_GET['email'];
+        $phoneNumber = $_GET['phoneNumber'];
+        $address = $_GET['address'];
+        $sql = "INSERT INTO customers (customerFirstName, customerLastName, customerDob, customerAddress, customerPhoneNumber, customerEmail) 
+        VALUES ('".$firstName."', '".$lastName."', '".$dob."', '".$address."', '".$phoneNumber."', '".$email."')";
+        // echo $sql;
+        // die();
+        // Execute the query
+        $result = mysqli_query($con, $sql);
+
+        if ($result) {
+            echo "Customer added successfully!";
+        } else {
+            echo "Error adding customer: " . mysqli_error($con);
+        }
+
+        // Close the database connection
+        mysqli_close($con);    
+    break;
+    case 'addRoom':
+        $roomname = $_GET['roomname'];
+        $roomtype = $_GET['roomtype'];
+        $roomrate = $_GET['roomrate'];
+        $roomstatus = $_GET['roomstatus'];
+        $sql = "INSERT INTO rooms (roomType, roomRate, roomStatus, roomName) 
+        VALUES ('".$roomtype."', '".$roomrate."', '".$roomstatus."', '".$roomname."')";
+        // echo $sql;
+        // die();
+        // Execute the query
+        $result = mysqli_query($con, $sql);
+
+        if ($result) {
+            echo "Room added successfully!";
+        } else {
+            echo "Error adding customer: " . mysqli_error($con);
+        }
+
+        // Close the database connection
+        mysqli_close($con);    
+    break;
+    case 'addBooking':
+        $customerID = $_GET['customerID'];
+        $roomid = $_GET['roomid'];
+        $checkoutdate = $_GET['checkoutdate'];
+        $checkindate = $_GET['checkindate'];
+        $paymentstatus = $_GET['paymentstatus'];
+        $guests = $_GET['guests'];
+
+        $sqlUpdateRoomStatus = "UPDATE rooms SET roomStatus = 'Reserved' WHERE roomID = '".$roomid."'";
+        $resultUpdateRoomStatus = mysqli_query($con, $sqlUpdateRoomStatus);
+
+        if (!$resultUpdateRoomStatus) {
+            throw new Exception("Error updating room status: " . mysqli_error($con));
+        }
+    
+        // Get room rate
+        $sqlGetRoomRate = "SELECT roomRate FROM rooms WHERE roomID = '".$roomid."'";
+        $resultGetRoomRate = mysqli_query($con, $sqlGetRoomRate);
+    
+        if (!$resultGetRoomRate) {
+            throw new Exception("Error getting room rate: " . mysqli_error($con));
+        }
+    
+        $row = mysqli_fetch_assoc($resultGetRoomRate);
+        $room_rate = $row['roomRate'];
+    
+        // Insert booking
+        $sqlInsertBooking = "INSERT INTO bookings (customerID, roomID, checkinDate, checkOutDate, totalAmount, paymentStatus, numberOfCustomer) 
+                            VALUES ('".$customerID."','".$roomid."','".$checkindate."','".$checkoutdate."','".$room_rate."','". $paymentstatus."','".$guests."')";
+        $resultInsertBooking = mysqli_query($con, $sqlInsertBooking);
+    
+        if ($resultInsertBooking) {
+            echo "Booking added successfully!";
+        } else {
+            echo "Error adding customer: " . mysqli_error($con);
+        }
+
+        // Close the database connection
+        mysqli_close($con);    
+    break;
+    case 'editBooking':
+        // Get values from the GET parameters
+        $bookingid = $_GET['bookingid'];
+        $roomid = $_GET['roomid'];
+        $checkoutdate = $_GET['checkoutdate'];
+        $checkindate = $_GET['checkindate'];
+        $paymentstatus = $_GET['paymentstatus'];
+        $guests = $_GET['guests'];
+        $message = $_GET['message'];
+        
+        $sql = "UPDATE bookings 
+                JOIN rooms ON bookings.roomID = rooms.roomID
+                SET 
+                bookings.roomID= '".$roomid."', 
+                checkinDate = '".$checkindate."', 
+                checkOutDate = '".$checkoutdate."', 
+                paymentStatus = '".$paymentstatus."', 
+                numberOfCustomer = '".$guests."', 
+                message = '".$message."' 
+                WHERE bookings.bookingID = '".$bookingid."'";
+
+        // Execute the query
+        $result = mysqli_query($con, $sql);
+
+        // Check for errors
+        if ($result) {
+            // Query executed successfully
+            echo "Update successful!";
+        } else {
+            // Query failed
+            echo "Error updating record: " . mysqli_error($con);
+        }
+    break;
+    case 'editCustomer':
+        // Get values from the GET parameters
+        $customerid = $_GET['customerid'];
+        $firstname = $_GET['firstname'];
+        $lastname = $_GET['lastname'];
+        $dob = $_GET['dob'];
+        $email = $_GET['email'];
+        $phone = $_GET['phone'];
+        $address = $_GET['address'];
+        
+        $sql = "UPDATE customers 
+            SET 
+                customerFirstName='".$firstname."',
+                customerLastName='".$lastname."',
+                customerDob='".$dob."',
+                customerAddress='".$address."',
+                customerPhoneNumber='".$phone."',
+                customerEmail='".$email."'
+            WHERE customerID = '".$customerid."'";
+
+        // Execute the query
+        $result = mysqli_query($con, $sql);
+
+        // Check for errors
+        if ($result) {
+            // Query executed successfully
+            echo "Update successful!";
+        } else {
+            // Query failed
+            echo "Error updating record: " . mysqli_error($con);
+        }
+    break;
+    case 'editRoom':
+        // Get values from the GET parameters
+        $roomid = $_GET['roomid'];
+        $roomname = $_GET['roomname'];
+        $roomtype = $_GET['roomtype'];
+        $roomrate = $_GET['roomrate'];
+        $roomstatus = $_GET['roomstatus'];
+        
+        $sql = "UPDATE rooms 
+                SET 
+                    roomType = '".$roomtype."' ,
+                    roomRate = '".$roomrate."' ,
+                    roomStatus = '".$roomstatus."' ,
+                    roomName = '".$roomname."' 
+                WHERE roomID = '". $roomid."'";
+        // Execute the query
+        $result = mysqli_query($con, $sql);
+
+        // Check for errors
+        if ($result) {
+            // Query executed successfully
+            echo "Update successful!";
+        } else {
+            // Query failed
+            echo "Error updating record: " . mysqli_error($con);
+        }
+    break;
+    default:
+        //  echo"can't reach";
+    break;
+} 
+
+
+
